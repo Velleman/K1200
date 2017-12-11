9a46dfcb66976db7895b5cc2dc250fef16bee313
Brecht Nuyttens <bn@velleman.be>
2017-12-11 11:42:06 +0100

    Updated description

--- examples/K1200_example/K1200_example.ino
+++ examples/K1200_example/K1200_example.ino
@@ -9,6 +9,11 @@
 
 	Board: LilyPad Arduino 
 	Processor: ATmega328
+
+	You'll need to install these libraries to compile the firmware.
+	https://github.com/rocketscream/Low-Power => needed for power management.
+	https://github.com/GreyGnome/EnableInterrupt => needed for interupt on any pin.
+	https://github.com/adafruit/RTClib => needed for managing the RTC Chip.
 */
 //------------------------------------------------------------------
 #define EI_ARDUINO_INTERRUPTED_PIN
